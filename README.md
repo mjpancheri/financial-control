9684099b83809c3aeb1d4f2f2d31a4516b73689f
Marcio Pancheri <mjpancheri@gmail.com>
2023-08-18 18:13:24 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Financial Control
+
+Simple Java Spring Boot RestAPI to control monthly financial expenses. Organized by date and kind, and with access control using JWT.
+
+## Configuration
+
+There are a Docker PostgreSQL in `compose.yaml`
---
+++ src/test/java/com/mjpancheri/financialcontrol/FinancialControlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mjpancheri.financialcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class FinancialControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
